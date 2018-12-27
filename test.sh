3f30c6cb8d7bd0e7936c9fed1b9ba95611b932ae
Intey <ziexe0@gmail.com>
2018-12-27 11:55:30 +0300

    [fix] correct test

--- test.sh
+++ test.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 http --json POST http://localhost:8000/api/users/ user:='{"username":"intey"}' &>/dev/null
 http --json POST http://localhost:8000/api/users/ user:='{"username":"dobby"}' &>/dev/null
-http --json POST http://localhost:8000/api/users/1/meets/ user:='{"meets": [{"data": "2018-11-11", "timeRange": "18:00-23:00"}]}'
+http --json POST http://localhost:8000/api/users/1/meets/ meets:='[{"data": "2018-11-11", "timeRange": "18:00-23:00"}]'
